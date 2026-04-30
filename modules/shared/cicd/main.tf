fbe8596df0d0166c5e32914a33a5dbeb590ff927
heekyeong lee <heekyeong8080@gmail.com>
2026-04-30 15:10:13 +0900

    fix(cicd): GitHub OIDC provider를 data source로 참조

--- modules/shared/cicd/main.tf
+++ modules/shared/cicd/main.tf
@@ -1,7 +1,5 @@
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
 }
 
 data "aws_caller_identity" "current" {}
@@ -14,7 +12,7 @@ resource "aws_iam_role" "github_actions" {
     Statement = [{
       Effect = "Allow"
       Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
+        Federated = data.aws_iam_openid_connect_provider.github.arn
       }
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
